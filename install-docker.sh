e51378682315b2a5f837f495cee32a7f570fa39b
branehaven <branehaven@protonmail.com>
2022-02-03 13:08:11 -0500

    Removed Docker Compose section

--- install-docker.sh
+++ install-docker.sh
@@ -29,14 +29,3 @@ sudo usermod -aG docker ${USER}
 
 # Apply the new group membership (without having to log out and back in):
 su - ${USER}
-
-# Install Docker Compose
-
-# Get the docker compose lates version number:
-# COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
-
-# Download version 2.2.3 of docker compose and save the executable file at /user/local/bin/docker-compose, which will make the software globally available as docker-compose:
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# Set correct permissions so that the docker-compose command is executable:
-sudo chmod +x /usr/local/bin/docker-compose
