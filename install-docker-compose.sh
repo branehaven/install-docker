136263703a675a37a722e800be268bbd3478ceae
branehaven <83501542+branehaven@users.noreply.github.com>
2022-02-11 13:40:47 -0500

    Update install-docker-compose.sh
    Revised for compose V2

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -3,8 +3,12 @@
 # Get the docker compose lates version number:
 # COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
 
-# Download version 2.2.3 of docker compose and save the executable file at /user/local/bin/docker-compose, which will make the software globally available as docker-compose:
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Download version 2.2.3 of docker compose and save the executable file at ~/.docker/docker-cli-plugins/:
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 
 # Set correct permissions so that the docker-compose command is executable:
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+
+Test your installation:
+docker compose version
